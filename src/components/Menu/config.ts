22a3bc6ce7f5d9619e9458898f6dad01a21efc3a
bourbondefi <bourbondefi@gmail.com>
2021-03-12 22:28:58 -0500

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -73,33 +73,33 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'Github',
-        href: 'https://github.com/goosedefi/',
+        href: 'https://github.com/bourbondefi/',
       },
       {
         label: 'Docs',
-        href: 'https://goosedefi.gitbook.io/goose-finance/',
+        href: 'https://bourbondefi.gitbook.io/goose-finance/',
       },
       {
         label: 'Blog',
-        href: 'https://goosefinance.medium.com/',
+        href: 'https://bourbonfinance.medium.com/',
       },
     ],
   },
-  {
-    label: 'Partnerships/IFO',
-    icon: 'GooseIcon',
-    href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
-  },
-  {
-    label: 'Audit by Hacken',
-    icon: 'AuditIcon',
-    href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
-  },
-  {
-    label: 'Audit by CertiK',
-    icon: 'AuditIcon',
-    href: 'https://certik.org/projects/goose-finance',
-  },
+ // {
+  //  label: 'Partnerships/IFO',
+//    icon: 'GooseIcon',
+ //   href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
+//  },
+ // {
+ //   label: 'Audit by Hacken',
+ //   icon: 'AuditIcon',
+ //   href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
+ // },
+ // {
+ //   label: 'Audit by CertiK',
+ //   icon: 'AuditIcon',
+  //  href: 'https://certik.org/projects/goose-finance',
+//  },
 ]
 
 export default config
