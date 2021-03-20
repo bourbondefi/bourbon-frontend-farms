27c6157a2381d519b32694ac6a3bf17f4cc3edab
bourbondefi <bourbondefi@gmail.com>
2021-03-20 19:42:11 -0400

    bourbonize

--- src/views/Lottery/Lottery.tsx
+++ src/views/Lottery/Lottery.tsx
@@ -31,7 +31,7 @@ const Lottery: React.FC = () => {
   const [mostRecentLotteryNumber, setMostRecentLotteryNumber] = useState(1)
 
   useEffect(() => {
-    fetch(`https://api.pancakeswap.com/api/lotteryHistory`)
+    fetch(`https://api.bourbondefi.com/lotteryHistory`)
       .then((response) => response.json())
       .then((data) => setHistoryData(data))
       .catch(() => {
