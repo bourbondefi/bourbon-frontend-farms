0be6f7b85027df399a07e7c22176209dc2253055
bourbondefi <80276717+bourbondefi@users.noreply.github.com>
2021-03-21 12:07:21 -0400

    bourbonize

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -314,40 +314,40 @@ const farms: FarmConfig[] = [
     quoteTokenSymbol: QuoteToken.BUSD,
     quoteTokenAdresses: contracts.busd,
   },
-  {
-    pid: 20,
-    risk: 3,
-    isTokenOnly: true,
-    lpSymbol: 'DOT',
-    lpAddresses: {
-      97: '',
-      56: '0x54c1ec2f543966953f2f7564692606ea7d5a184e', // DOT-BUSD LP
-    },
-    tokenSymbol: 'DOT',
-    tokenAddresses: {
-      97: '',
-      56: '0x7083609fce4d1d8dc0c979aab8c869ea2c873402',
-    },
-    quoteTokenSymbol: QuoteToken.BUSD,
-    quoteTokenAdresses: contracts.busd,
-  },
-  {
-    pid: 21,
-    risk: 4,
-    isTokenOnly: true,
-    lpSymbol: 'CAKE',
-    lpAddresses: {
-      97: '',
-      56: '0x0ed8e0a2d99643e1e65cca22ed4424090b8b7458', // CAKE-BUSD LP
-    },
-    tokenSymbol: 'CAKE',
-    tokenAddresses: {
-      97: '',
-      56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-    },
-    quoteTokenSymbol: QuoteToken.BUSD,
-    quoteTokenAdresses: contracts.busd,
-  },
+//  {
+//    pid: 20,
+//    risk: 3,
+//    isTokenOnly: true,
+//    lpSymbol: 'DOT',
+//    lpAddresses: {
+//      97: '',
+//      56: '0x54c1ec2f543966953f2f7564692606ea7d5a184e', // DOT-BUSD LP
+//    },
+//    tokenSymbol: 'DOT',
+//    tokenAddresses: {
+//      97: '',
+//      56: '0x7083609fce4d1d8dc0c979aab8c869ea2c873402',
+//    },
+//    quoteTokenSymbol: QuoteToken.BUSD,
+//    quoteTokenAdresses: contracts.busd,
+//  },
+//  {
+//    pid: 21,
+//    risk: 4,
+//   isTokenOnly: true,
+//    lpSymbol: 'CAKE',
+//    lpAddresses: {
+//      97: '',
+//      56: '0x0ed8e0a2d99643e1e65cca22ed4424090b8b7458', // CAKE-BUSD LP
+//    },
+//    tokenSymbol: 'CAKE',
+//    tokenAddresses: {
+//      97: '',
+//      56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+//    },
+//    quoteTokenSymbol: QuoteToken.BUSD,
+//    quoteTokenAdresses: contracts.busd,
+//  },
 //  {
 //    pid: 22,
 //    risk: 3,
