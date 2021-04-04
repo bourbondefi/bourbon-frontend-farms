0c807d70be5909583c0d33517c8cea19aa7ecac2
bourbondefi <80276717+bourbondefi@users.noreply.github.com>
2021-04-04 00:59:20 -0400

    add pools

--- src/views/Pools/components/Coming.tsx
+++ src/views/Pools/components/Coming.tsx
@@ -49,7 +49,7 @@ const Coming: React.FC = () => {
         <Button
           variant="secondary"
           as="a"
-          href="https://docs.google.com/forms/d/e/1FAIpQLScGdT5rrVMr4WOWr08pvcroSeuIOtEJf1sVdQGVdcAOqryigQ/viewform"
+          href=""
           external
           fullWidth
           mb="16px"
@@ -67,7 +67,7 @@ const Coming: React.FC = () => {
             </span>
             {TranslateString(384, 'Your Stake')}:
           </div>
-          <Value>??? CAKE</Value>
+          <Value>??? BRRL</Value>
         </DetailPlaceholder>
       </div>
       <Footer>
