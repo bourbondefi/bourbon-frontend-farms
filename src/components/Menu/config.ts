41fb0f46ab17ebc9495a42b5a3ae883697914c7e
bourbondefi <80276717+bourbondefi@users.noreply.github.com>
2021-04-08 15:57:29 -0400

    update menu

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -2,66 +2,62 @@ import { MenuEntry } from '@pancakeswap-libs/uikit'
 
 const config: MenuEntry[] = [
   {
-    label: 'Home',
+    label: 'Bourbon Stats',
     icon: 'HomeIcon',
-    href: '/',
+    href: 'https://www.bourbondefi.com/'
+  },
+  {
+    label: 'Rare Bourbon Stats',
+    icon: 'HomeIcon',
+    href: 'https://farms.bourbondefi.com/'
   },
   {
     label: 'Trade',
     icon: 'TradeIcon',
+    initialOpenState: true,
     items: [
       {
         label: 'Exchange',
-        href: 'https://exchange.bourbondefi.com/',
+        href: '/swap'
       },
       {
         label: 'Liquidity',
-        href: 'https://exchange.bourbondefi.com/#/pool',
-      },
-    ],
+        href: '/pool'
+      }
+    ]
   },
   {
     label: 'Farms',
     icon: 'FarmIcon',
-    href: '/farms',
+    href: 'https://www.bourbondefi.com/farms'
   },
   {
     label: 'Rare Farms',
     icon: 'FarmIcon',
-    href: 'https://farms.bourbondefi.com/farms',
+    href: 'https://farms.bourbondefi.com/farms'
   },
   {
     label: 'Distillery',
     icon: 'PoolIcon',
-    href: '/nests',
+    href: 'https://www.bourbondefi.com/nests'
   },
   {
     label: 'Rare Distillery',
     icon: 'PoolIcon',
-    href: 'https://farms.bourbondefi.com/nests',
+    href: 'https://farms.bourbondefi.com/nests'
   },
-  // {
-  //   label: 'Pools',
-  //   icon: 'PoolIcon',
-  //   href: '/pools',
-  // },
   // {
   //   label: 'Lottery',
   //   icon: 'TicketIcon',
-  //   href: '/lottery',
-  // },
-  // {
-  //   label: 'NFT',
-  //   icon: 'NftIcon',
-  //   href: '/nft',
+  //   href: 'https://www.bourbondefi.com/lottery'
   // },
   {
     label: 'Bourbon Barrel Token',
     icon: 'InfoIcon',
     items: [
       {
-        label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0x1b30e60130b364DDfC0267525d7D8039d795FB33',
+        label: 'Dex Guru',
+        href: 'https://dex.guru/token/0xcbe73dd7e8fc74011136b837a59205801c45e6a1-bsc',
       },
       {
         label: 'Contract',
@@ -82,8 +78,8 @@ const config: MenuEntry[] = [
     icon: 'InfoIcon',
     items: [
       {
-        label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0x6392aC63A851497Fc1F86946923a365162518E30',
+        label: 'Dex Guru',
+        href: 'https://dex.guru/token/0x7595bb34bf391c3de0a6878a36d06223b2bfd404-bsc',
       },
      {
         label: 'Contract',
@@ -95,7 +91,7 @@ const config: MenuEntry[] = [
     //  },
       {
         label: 'AstroTools',
-        href: 'https://app.astrotools.io/pancake-pair-explorer/0x6392aC63A851497Fc1F86946923a365162518E30',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0x3F8F14Dd4Ef2a2DBD3C1B87f96EdD028663c76b1',
       },
     ],
   },
@@ -119,19 +115,19 @@ const config: MenuEntry[] = [
   },
  // {
   //  label: 'Partnerships/IFO',
-//    icon: '',
+ //   icon: '',
  //   href: '',
 //  },
- // {
- //   label: 'Audit by Hacken',
- //   icon: 'AuditIcon',
- //   href: '',
- // },
+  {
+    label: 'Audit by Techrate',
+    icon: 'AuditIcon',
+    href: 'https://www.bourbondefi.com/files/hackenAudit.pdf',
+  },
  // {
  //   label: 'Audit by CertiK',
  //   icon: 'AuditIcon',
-  //  href: '',
-//  },
+ //   href: '',
+ // },
 ]
 
 export default config
