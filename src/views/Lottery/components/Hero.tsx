296c4b70e7f38f51212e3cd5246bc7705b2a87b1
bourbondefi <80276717+bourbondefi@users.noreply.github.com>
2021-04-12 12:25:23 -0400

    Update Graphics Move Derby Date
    De egg and move derby to weekly

--- src/views/Lottery/components/Hero.tsx
+++ src/views/Lottery/components/Hero.tsx
@@ -75,7 +75,7 @@ const Hero = () => {
           <Blurb>{TranslateString(999, 'Win if 2, 3, or 4 of your horses match!')}</Blurb>
         </LeftWrapper>
         <RightWrapper>
-          <LotteryProgress />
+          <text>Next Bourbon Derby Will Run on: April 16, 2021 at 7PM EST</text>
         </RightWrapper>
       </StyledContainer>
     </StyledHero>
