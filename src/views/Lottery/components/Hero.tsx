896184652657d84e436d72750340e7d3cf6d081d
bourbondefi <80276717+bourbondefi@users.noreply.github.com>
2021-04-16 19:18:08 -0400

    Update Hero.tsx

--- src/views/Lottery/components/Hero.tsx
+++ src/views/Lottery/components/Hero.tsx
@@ -75,7 +75,7 @@ const Hero = () => {
           <Blurb>{TranslateString(999, 'Win if 2, 3, or 4 of your horses match!')}</Blurb>
         </LeftWrapper>
         <RightWrapper>
-          <text>Next Bourbon Derby Will Run on: April 16, 2021 at 7PM EST</text>
+          <text>Next Bourbon Derby Will Run on: April 23, 2021 at 7PM EST</text>
         </RightWrapper>
       </StyledContainer>
     </StyledHero>
